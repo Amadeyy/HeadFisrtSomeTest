41460e157140847082429b5bdc2ccf13904a6eff
Amadeyy <miha123560@gmail.com>
2022-04-30 20:25:05 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            testFunc2();
+        }
+
+        private static void testFunc2()
+        {
+            Random random = new Random();
+            double[] randomDoubles = new double[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                randomDoubles[i] = random.NextDouble();
+            }
+
+            int count = 0;
+            foreach (var item in randomDoubles)
+            {
+                Console.Write($"# {++count} = ");
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void testFunc()
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+    }
+}
